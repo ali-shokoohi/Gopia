958871cfe96476760415148f1cf121765347b75b
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-03-26 16:03:21 +0430

    Create Atricles struct type and show objects of them to viewers

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Descriptions"`
+	Content string `json:"Content"`
+}
+
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+	Articles = []Article{
+		Article{Title: "1984", Desc: "Article of 1984 book", Content: "This book is wonderful"},
+		Article{Title: "Homo sapiens", Desc: "Article of Homo sapiens book", Content: "This book is so useful"},
+	}
 	handleRequests()
 }
