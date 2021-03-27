3cb44682030a0c57b25e6defc689ec8ab8a0161d
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-03-27 21:47:55 +0430

    Connect to postgresql database

---
+++ db.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "go_user"
+	password = "pag%5!%zhQ*cjGr^orjZfKC*V65HhPb5"
+	dbname   = "go_api"
+)
+
+func get_db() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+
+	return db
+}
