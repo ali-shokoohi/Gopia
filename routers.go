4770ccae9dd6d2ad9bcd8dbd90f9c47afb41d120
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-03 20:23:17 +0430

    Move routers to routers.go file

---
+++ routers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/article", returnAllArticles).Methods("GET")
+	router.HandleFunc("/article", createNewArticle).Methods("POST")
+	router.HandleFunc("/article/{id}", returnSingleArticle).Methods("GET")
+	router.HandleFunc("/article/{id}", deleteSingleArticle).Methods("DELETE")
+	router.HandleFunc("/article/{id}", updateSingleArticle).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
