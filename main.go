29f7af1853bf36029b005913e4f42ba2aa5cb860
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-04 14:39:08 +0430

    Define vars in controllers.go is better choice

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 )
 
-var Articles []Article
-
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
-	models = perpareModels()
-	db.Find(&Articles)
+	models, objects = perpareModels()
 	handleRequests()
 }
