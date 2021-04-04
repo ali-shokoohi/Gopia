c7192f78bb356d54a03c0f1b68e9685cb119cdc1
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-04 19:14:13 +0430

    Make Basic Authentication middleware

--- middlewares.go
+++ middlewares.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"net/http"
 )
@@ -9,5 +11,30 @@ func urlMiddleWare(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Request at: %v\n", r.URL)
 		handler.ServeHTTP(w, r)
+	})
+}
+
+func authMiddleWare(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Ignore GET method
+		if r.Method == "GET" {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		reqUser, reqPass, ok := r.BasicAuth()
+		if !ok {
+			http.Error(w, "Access Dinied!", http.StatusForbidden)
+			return
+		}
+		hasher := md5.New()
+		hasher.Write([]byte(reqPass))
+		hashPass := hex.EncodeToString(hasher.Sum(nil))
+		for _, user := range Users {
+			if user.Username == reqUser && user.Password == hashPass {
+				handler.ServeHTTP(w, r)
+				return
+			}
+		}
+		http.Error(w, "Access Dinied!", http.StatusForbidden)
 	})
 }
