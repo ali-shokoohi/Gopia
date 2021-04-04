e104246a9c8ec2473d223b6a41088be4da8b0cc0
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-04 16:40:48 +0430

    Add User database model

--- models.go
+++ models.go
@@ -15,7 +15,22 @@ type Article struct {
 	Content string `gorm:"not null" json:"Content"`
 }
 
+var Articles []Article
+
+type User struct {
+	gorm.Model
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Email     string `gorm:"not null" json:"email"`
+	Age       string `gorm:"not null" json:"age"`
+	Username  string `gorm:"not null" json:"username"`
+	Password  string `gorm:"not null" json:"password"`
+}
+
+var Users []User
+
 var models map[string]interface{}
+var objects map[string]interface{}
 
 func autoMigrate(models map[string]interface{}) {
 	for index, model := range models {
@@ -24,9 +39,15 @@ func autoMigrate(models map[string]interface{}) {
 	}
 }
 
-func perpareModels() map[string]interface{} {
+func perpareModels() (map[string]interface{}, map[string]interface{}) {
 	models = make(map[string]interface{})
+	objects = make(map[string]interface{})
 	models["article"] = Article{}
+	models["user"] = User{}
 	autoMigrate(models)
-	return models
+	db.Find(&Articles)
+	db.Find(&Users)
+	objects["articles"] = Articles
+	objects["users"] = Users
+	return models, objects
 }
