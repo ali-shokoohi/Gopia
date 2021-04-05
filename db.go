9aab1d4263446b383de02efdc0d01c59232caa47
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-05 20:05:32 +0430

    Change environments names

--- db.go
+++ db.go
@@ -11,18 +11,18 @@ import (
 )
 
 var (
-	_        = godotenv.Load()
-	host     = os.Getenv("host")
-	port     = os.Getenv("port")
-	user     = os.Getenv("user")
-	password = os.Getenv("password")
-	dbname   = os.Getenv("dbname")
+	_          = godotenv.Load()
+	dbhost     = os.Getenv("dbhost")
+	dbport     = os.Getenv("dbport")
+	dbuser     = os.Getenv("dbuser")
+	dbpassword = os.Getenv("dbpassword")
+	dbname     = os.Getenv("dbname")
 )
 
 func getDatabase() *gorm.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=require TimeZone=Asia/Tehran",
-		host, port, user, password, dbname)
+		dbhost, dbport, dbuser, dbpassword, dbname)
 
 	//db, err := sql.Open("postgres", psqlInfo)
 	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
