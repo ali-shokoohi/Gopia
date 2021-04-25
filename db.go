3e25c17de0a969a9b91f2a6696ac7256c57db53c
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-25 12:34:29 +0430

    Convert tabs to spaces

--- db.go
+++ db.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/joho/godotenv"
+    "github.com/joho/godotenv"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 var (
-	_          = godotenv.Load()
-	dbhost     = os.Getenv("dbhost")
-	dbport     = os.Getenv("dbport")
-	dbuser     = os.Getenv("dbuser")
-	dbpassword = os.Getenv("dbpassword")
-	dbname     = os.Getenv("dbname")
+    _          = godotenv.Load()
+    dbhost     = os.Getenv("dbhost")
+    dbport     = os.Getenv("dbport")
+    dbuser     = os.Getenv("dbuser")
+    dbpassword = os.Getenv("dbpassword")
+    dbname     = os.Getenv("dbname")
 )
 
 func getDatabase() *gorm.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=require TimeZone=Asia/Tehran",
-		dbhost, dbport, dbuser, dbpassword, dbname)
+    psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+        "password=%s dbname=%s sslmode=require TimeZone=Asia/Tehran",
+        dbhost, dbport, dbuser, dbpassword, dbname)
 
-	//db, err := sql.Open("postgres", psqlInfo)
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+    //db, err := sql.Open("postgres", psqlInfo)
+    db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+    if err != nil {
+        panic(err)
+    }
 
-	fmt.Println("Successfully connected to database!")
+    fmt.Println("Successfully connected to database!")
 
-	return db
+    return db
 }
--- models.go
+++ models.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"strings"
+    "crypto/md5"
+    "encoding/hex"
+    "encoding/json"
+    "fmt"
+    "strings"
 
-	jwt "github.com/dgrijalva/jwt-go"
-	"gorm.io/gorm"
+    jwt "github.com/dgrijalva/jwt-go"
+    "gorm.io/gorm"
 )
 
 var db = getDatabase()
 
 type Article struct {
-	gorm.Model
-	Title   string `gorm:"not null" json:"Title"`
-	Desc    string `gorm:"not null" json:"Descriptions"`
-	Content string `gorm:"not null" json:"Content"`
-	UserID  uint   `gorm:"default:1"`
+    gorm.Model
+    Title   string `gorm:"not null" json:"Title"`
+    Desc    string `gorm:"not null" json:"Descriptions"`
+    Content string `gorm:"not null" json:"Content"`
+    UserID  uint   `gorm:"default:1"`
 }
 
 var Articles []Article
 
 type User struct {
-	gorm.Model
-	FirstName string    `gorm:"not null" json:"first_name"`
-	LastName  string    `gorm:"not null" json:"last_name"`
-	Email     string    `gorm:"not null;unique" json:"email"`
-	Age       string    `gorm:"not null" json:"age"`
-	Username  string    `gorm:"not null;unique"`
-	Password  string    `gorm:"not null"`
-	Articles  []Article `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+    gorm.Model
+    FirstName string    `gorm:"not null" json:"first_name"`
+    LastName  string    `gorm:"not null" json:"last_name"`
+    Email     string    `gorm:"not null;unique" json:"email"`
+    Age       string    `gorm:"not null" json:"age"`
+    Username  string    `gorm:"not null;unique"`
+    Password  string    `gorm:"not null"`
+    Articles  []Article `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 var Users []User
@@ -40,8 +40,8 @@ var Users []User
 JWT claims struct
 */
 type Token struct {
-	UserId uint
-	jwt.StandardClaims
+    UserId uint
+    jwt.StandardClaims
 }
 
 var models map[string]interface{}
@@ -52,93 +52,93 @@ var objectsJson []byte
 
 //Validate incoming user details...
 func (user *User) Validate() (string, bool) {
-	if !strings.Contains(user.Email, "@") {
-		return "Email address is required", false
-	}
-	if len(user.Password) < 6 {
-		return "Strong Password is required", false
-	}
-	//Email and Username must be unique
-	temp := &User{}
-	//check for errors and duplicate emails
-	err := db.Table("users").Where("email = ?", user.Email).First(temp).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return "Connection error. Please retry", false
-	}
-	if temp.Email != "" {
-		return "Email address already in use by another user.", false
-	}
-	errUser := db.Table("users").Where("Username = ?", user.Username).First(temp).Error
-	if errUser != nil && errUser != gorm.ErrRecordNotFound {
-		return "Connection error. Please retry", false
-	}
-	if temp.Username != "" {
-		return "Username already in use by another user.", false
-	}
-	return "Requirement passed", true
+    if !strings.Contains(user.Email, "@") {
+        return "Email address is required", false
+    }
+    if len(user.Password) < 6 {
+        return "Strong Password is required", false
+    }
+    //Email and Username must be unique
+    temp := &User{}
+    //check for errors and duplicate emails
+    err := db.Table("users").Where("email = ?", user.Email).First(temp).Error
+    if err != nil && err != gorm.ErrRecordNotFound {
+        return "Connection error. Please retry", false
+    }
+    if temp.Email != "" {
+        return "Email address already in use by another user.", false
+    }
+    errUser := db.Table("users").Where("Username = ?", user.Username).First(temp).Error
+    if errUser != nil && errUser != gorm.ErrRecordNotFound {
+        return "Connection error. Please retry", false
+    }
+    if temp.Username != "" {
+        return "Username already in use by another user.", false
+    }
+    return "Requirement passed", true
 }
 
 func (user *User) Create() (string, bool) {
-	if resp, ok := user.Validate(); !ok {
-		return resp, false
-	}
-	hasher := md5.New()
-	hasher.Write([]byte(user.Password))
-	user.Password = hex.EncodeToString(hasher.Sum(nil))
-	db.Create(&user)
-	return "Account has been created", true
+    if resp, ok := user.Validate(); !ok {
+        return resp, false
+    }
+    hasher := md5.New()
+    hasher.Write([]byte(user.Password))
+    user.Password = hex.EncodeToString(hasher.Sum(nil))
+    db.Create(&user)
+    return "Account has been created", true
 }
 
 func (user *User) Update() (string, bool) {
-	hasher := md5.New()
-	hasher.Write([]byte(user.Password))
-	user.Password = hex.EncodeToString(hasher.Sum(nil))
-	db.Save(&user)
-	return "Account has been updated", true
+    hasher := md5.New()
+    hasher.Write([]byte(user.Password))
+    user.Password = hex.EncodeToString(hasher.Sum(nil))
+    db.Save(&user)
+    return "Account has been updated", true
 }
 
 func (user User) MarshalJSON() ([]byte, error) {
-	var tmp struct {
-		ID        uint
-		FirstName string    `json:"first_name"`
-		LastName  string    `json:"last_name"`
-		Email     string    `json:"email"`
-		Age       string    `json:"age"`
-		Articles  []Article `json:"articles"`
-	}
-	tmp.ID = user.ID
-	tmp.FirstName = user.FirstName
-	tmp.LastName = user.LastName
-	tmp.Email = user.Email
-	tmp.Age = user.Age
-	tmp.Articles = user.Articles
-	return json.Marshal(&tmp)
+    var tmp struct {
+        ID        uint
+        FirstName string    `json:"first_name"`
+        LastName  string    `json:"last_name"`
+        Email     string    `json:"email"`
+        Age       string    `json:"age"`
+        Articles  []Article `json:"articles"`
+    }
+    tmp.ID = user.ID
+    tmp.FirstName = user.FirstName
+    tmp.LastName = user.LastName
+    tmp.Email = user.Email
+    tmp.Age = user.Age
+    tmp.Articles = user.Articles
+    return json.Marshal(&tmp)
 }
 
 func autoMigrate(models map[string]interface{}) {
-	for index, model := range models {
-		fmt.Printf("%s: %v\n", index, model)
-		db.AutoMigrate(model)
-	}
+    for index, model := range models {
+        fmt.Printf("%s: %v\n", index, model)
+        db.AutoMigrate(model)
+    }
 }
 
 func reloadObjects() {
-	// Convert objects map to a []byte map
-	objectsJson, _ = json.Marshal(objects)
-	// Again convert to a string map
-	json.Unmarshal(objectsJson, &objectsJsonMap)
+    // Convert objects map to a []byte map
+    objectsJson, _ = json.Marshal(objects)
+    // Again convert to a string map
+    json.Unmarshal(objectsJson, &objectsJsonMap)
 }
 
 func perpareModels() (map[string]interface{}, map[string]interface{}) {
-	models = make(map[string]interface{})
-	objects = make(map[string]interface{})
-	models["article"] = Article{}
-	models["user"] = User{}
-	autoMigrate(models)
-	db.Find(&Articles)
-	db.Preload("Articles").Find(&Users)
-	objects["articles"] = Articles
-	objects["users"] = Users
-	reloadObjects()
-	return models, objects
+    models = make(map[string]interface{})
+    objects = make(map[string]interface{})
+    models["article"] = Article{}
+    models["user"] = User{}
+    autoMigrate(models)
+    db.Find(&Articles)
+    db.Preload("Articles").Find(&Users)
+    objects["articles"] = Articles
+    objects["users"] = Users
+    reloadObjects()
+    return models, objects
 }
