5684e2ee9da4b8b7f738574ea02597f2d1573db7
Sabzi Greenly <7986490-greenly@users.noreply.gitlab.com>
2021-04-27 15:58:48 +0430

    Create jwt token field for every users

--- models.go
+++ models.go
@@ -1,37 +1,39 @@
 package main
 
 import (
-    "crypto/md5"
-    "encoding/hex"
-    "encoding/json"
-    "fmt"
-    "strings"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
 
-    jwt "github.com/dgrijalva/jwt-go"
-    "gorm.io/gorm"
+	jwt "github.com/dgrijalva/jwt-go"
+	"gorm.io/gorm"
 )
 
 var db = getDatabase()
 
 type Article struct {
-    gorm.Model
-    Title   string `gorm:"not null" json:"Title"`
-    Desc    string `gorm:"not null" json:"Descriptions"`
-    Content string `gorm:"not null" json:"Content"`
-    UserID  uint   `gorm:"default:1"`
+	gorm.Model
+	Title   string `gorm:"not null" json:"Title"`
+	Desc    string `gorm:"not null" json:"Descriptions"`
+	Content string `gorm:"not null" json:"Content"`
+	UserID  uint   `gorm:"default:1"`
 }
 
 var Articles []Article
 
 type User struct {
-    gorm.Model
-    FirstName string    `gorm:"not null" json:"first_name"`
-    LastName  string    `gorm:"not null" json:"last_name"`
-    Email     string    `gorm:"not null;unique" json:"email"`
-    Age       string    `gorm:"not null" json:"age"`
-    Username  string    `gorm:"not null;unique"`
-    Password  string    `gorm:"not null"`
-    Articles  []Article `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	gorm.Model
+	FirstName string    `gorm:"not null" json:"first_name"`
+	LastName  string    `gorm:"not null" json:"last_name"`
+	Email     string    `gorm:"not null;unique" json:"email"`
+	Age       string    `gorm:"not null" json:"age"`
+	Username  string    `gorm:"not null;unique"`
+	Password  string    `gorm:"not null"`
+	Articles  []Article `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Token     string    `gorm:"" json:"token"`
 }
 
 var Users []User
@@ -40,8 +42,8 @@ var Users []User
 JWT claims struct
 */
 type Token struct {
-    UserId uint
-    jwt.StandardClaims
+	UserId uint
+	jwt.StandardClaims
 }
 
 var models map[string]interface{}
@@ -52,93 +54,100 @@ var objectsJson []byte
 
 //Validate incoming user details...
 func (user *User) Validate() (string, bool) {
-    if !strings.Contains(user.Email, "@") {
-        return "Email address is required", false
-    }
-    if len(user.Password) < 6 {
-        return "Strong Password is required", false
-    }
-    //Email and Username must be unique
-    temp := &User{}
-    //check for errors and duplicate emails
-    err := db.Table("users").Where("email = ?", user.Email).First(temp).Error
-    if err != nil && err != gorm.ErrRecordNotFound {
-        return "Connection error. Please retry", false
-    }
-    if temp.Email != "" {
-        return "Email address already in use by another user.", false
-    }
-    errUser := db.Table("users").Where("Username = ?", user.Username).First(temp).Error
-    if errUser != nil && errUser != gorm.ErrRecordNotFound {
-        return "Connection error. Please retry", false
-    }
-    if temp.Username != "" {
-        return "Username already in use by another user.", false
-    }
-    return "Requirement passed", true
+	if !strings.Contains(user.Email, "@") {
+		return "Email address is required", false
+	}
+	if len(user.Password) < 6 {
+		return "Strong Password is required", false
+	}
+	//Email and Username must be unique
+	temp := &User{}
+	//check for errors and duplicate emails
+	err := db.Table("users").Where("email = ?", user.Email).First(temp).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return "Connection error. Please retry", false
+	}
+	if temp.Email != "" {
+		return "Email address already in use by another user.", false
+	}
+	errUser := db.Table("users").Where("Username = ?", user.Username).First(temp).Error
+	if errUser != nil && errUser != gorm.ErrRecordNotFound {
+		return "Connection error. Please retry", false
+	}
+	if temp.Username != "" {
+		return "Username already in use by another user.", false
+	}
+	return "Requirement passed", true
 }
 
 func (user *User) Create() (string, bool) {
-    if resp, ok := user.Validate(); !ok {
-        return resp, false
-    }
-    hasher := md5.New()
-    hasher.Write([]byte(user.Password))
-    user.Password = hex.EncodeToString(hasher.Sum(nil))
-    db.Create(&user)
-    return "Account has been created", true
+	if resp, ok := user.Validate(); !ok {
+		return resp, false
+	}
+	hasher := md5.New()
+	hasher.Write([]byte(user.Password))
+	user.Password = hex.EncodeToString(hasher.Sum(nil))
+	db.Create(&user)
+	if user.ID <= 0 {
+		return "Failed to create account, connection error.", false
+	}
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	user.Token = tokenString
+	return "Account has been created", true
 }
 
 func (user *User) Update() (string, bool) {
-    hasher := md5.New()
-    hasher.Write([]byte(user.Password))
-    user.Password = hex.EncodeToString(hasher.Sum(nil))
-    db.Save(&user)
-    return "Account has been updated", true
+	hasher := md5.New()
+	hasher.Write([]byte(user.Password))
+	user.Password = hex.EncodeToString(hasher.Sum(nil))
+	db.Save(&user)
+	return "Account has been updated", true
 }
 
 func (user User) MarshalJSON() ([]byte, error) {
-    var tmp struct {
-        ID        uint
-        FirstName string    `json:"first_name"`
-        LastName  string    `json:"last_name"`
-        Email     string    `json:"email"`
-        Age       string    `json:"age"`
-        Articles  []Article `json:"articles"`
-    }
-    tmp.ID = user.ID
-    tmp.FirstName = user.FirstName
-    tmp.LastName = user.LastName
-    tmp.Email = user.Email
-    tmp.Age = user.Age
-    tmp.Articles = user.Articles
-    return json.Marshal(&tmp)
+	var tmp struct {
+		ID        uint
+		FirstName string    `json:"first_name"`
+		LastName  string    `json:"last_name"`
+		Email     string    `json:"email"`
+		Age       string    `json:"age"`
+		Articles  []Article `json:"articles"`
+	}
+	tmp.ID = user.ID
+	tmp.FirstName = user.FirstName
+	tmp.LastName = user.LastName
+	tmp.Email = user.Email
+	tmp.Age = user.Age
+	tmp.Articles = user.Articles
+	return json.Marshal(&tmp)
 }
 
 func autoMigrate(models map[string]interface{}) {
-    for index, model := range models {
-        fmt.Printf("%s: %v\n", index, model)
-        db.AutoMigrate(model)
-    }
+	for index, model := range models {
+		fmt.Printf("%s: %v\n", index, model)
+		db.AutoMigrate(model)
+	}
 }
 
 func reloadObjects() {
-    // Convert objects map to a []byte map
-    objectsJson, _ = json.Marshal(objects)
-    // Again convert to a string map
-    json.Unmarshal(objectsJson, &objectsJsonMap)
+	// Convert objects map to a []byte map
+	objectsJson, _ = json.Marshal(objects)
+	// Again convert to a string map
+	json.Unmarshal(objectsJson, &objectsJsonMap)
 }
 
 func perpareModels() (map[string]interface{}, map[string]interface{}) {
-    models = make(map[string]interface{})
-    objects = make(map[string]interface{})
-    models["article"] = Article{}
-    models["user"] = User{}
-    autoMigrate(models)
-    db.Find(&Articles)
-    db.Preload("Articles").Find(&Users)
-    objects["articles"] = Articles
-    objects["users"] = Users
-    reloadObjects()
-    return models, objects
+	models = make(map[string]interface{})
+	objects = make(map[string]interface{})
+	models["article"] = Article{}
+	models["user"] = User{}
+	autoMigrate(models)
+	db.Find(&Articles)
+	db.Preload("Articles").Find(&Users)
+	objects["articles"] = Articles
+	objects["users"] = Users
+	reloadObjects()
+	return models, objects
 }
