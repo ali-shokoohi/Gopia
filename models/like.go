0b50e81f064908f7f4a41e32cc4eaa5ad17762c5
Sabzi Greenly <javadi@tutanota.com>
2021-08-20 15:48:31 +0430

    Create new Like gorm database model

---
+++ models/like.go
@@ -0,0 +1,14 @@
+package models
+
+import "gorm.io/gorm"
+
+// Like - gorm database model type
+type Like struct {
+	gorm.Model
+	UserID    uint `gorm:"not null"`
+	ArticleID uint
+	CommentID uint
+}
+
+// Likes - List of all likes
+var Likes []Like
